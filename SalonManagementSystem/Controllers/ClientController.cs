2cc83b2457870a0fa9ca9ac4c84ee8f3c025e007
Urooba Batool <162785493+Urooba-Batool@users.noreply.github.com>
2026-05-04 16:07:34 +0500

    Add files via upload

---
+++ SalonManagementSystem/Controllers/ClientController.cs
@@ -0,0 +1,36 @@
+﻿using SalonManagementSystem.DAL;
+using SalonManagementSystem.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalonManagementSystem.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly DBHelper _db;
+
+        public ClientController(DBHelper db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Client c)
+        {
+            SqlParameter[] p =
+            {
+                new SqlParameter("@ClientName", c.ClientName),
+                new SqlParameter("@ClientPhone", c.ClientPhone)
+            };
+
+            _db.Execute("sp_AddClient", p);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SalonManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalonManagementSystem.DAL;
+using SalonManagementSystem.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace SalonManagementSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly DBHelper _db;
+
+        public LoginController(DBHelper db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Login l)
+        {
+            SqlParameter[] p =
+            {
+                new SqlParameter("@Role", l.UserRole),
+                new SqlParameter("@Password", l.UserPassword)
+            };
+
+            DataTable dt = _db.GetData("sp_LoginUser", p);
+
+            if (dt.Rows.Count > 0)
+            {
+                string role = dt.Rows[0]["UserRole"].ToString();
+
+                if (role == "Admin")
+                    return RedirectToAction("AdminDashboard");
+
+                else
+                    return RedirectToAction("StaffDashboard");
+            }
+
+            ViewBag.Error = "Invalid login!";
+            return View();
+        }
+
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+        public IActionResult StaffDashboard()
+        {
+            return View();
+        }
+    }
+}
